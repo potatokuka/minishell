e590bdcc067eb937bf8951171cb65a30841c56e2
griffin mbp <griffin.reed3@gmail.com>
2020-06-14 18:30:25 +0300

    Chore: Adj Make

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: greed <greed@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/06/14 18:26:52 by greed         #+#    #+#                 */
+/*   Updated: 2020/06/14 18:26:53 by greed         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/* first step, print out PROMPT and allow for exit */
+
+int	main(void)
+{
+  init_env();
+
+  return (0);
+}
