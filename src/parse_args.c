9262bea61159e14488a16d29dd574223c6b97811
MrOsteoporosis <sanderverheijen@outlook.com>
2020-09-18 17:47:06 +0200

    better enviroment variable subtitution

--- src/parse_args.c
+++ src/parse_args.c
@@ -6,74 +6,11 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/21 11:07:59 by greed         #+#    #+#                 */
-/*   Updated: 2020/09/18 17:09:15 by averheij      ########   odam.nl         */
+/*   Updated: 2020/09/18 17:46:06 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/*
-** need to trim spaces until first non ' '
-** CMD == from first CHAR -> ' '
-** after this is saved, the DATA input NEEDS to be trim
-** EVERY ' ' EXCEPT the FIRST one AFTER the last CHAR
-*/
-
-/* 
-** split arg_lst into argc argv for easier access
-*/
-
-int		ft_env_char(int c)
-{
-	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_');
-}
-
-/*
-** When triggered by a '$', save through to the next '$' || '\0' || ' '
-** IF the characters inside != UPPER CASE APHLA dont save shit inside
-*/
-
-char	*ft_save_dolla(t_data *data, char *trimmed, int start)
-{
-	char	*str;
-	int		i;
-	int		skip;
-
-	skip = 0;
-	i = 0;
-	while (trimmed[start] != '$' && trimmed[start] != ' ' &&
-				trimmed[start] != '\0')
-	{
-		if (ft_env_char(trimmed[start]) == 0)
-			skip = 1;
-		i++;
-		start++;
-	}
-	if (skip == 0)
-	{
-		str = ft_strldup(trimmed, i + 1);
-		if (!str)
-			put_error("Error in arguement parsing");
-		printf("STR $ check =_%s_\n", str);
-		if (*str)
-		{
-			data->argc += 1;
-			lst_new_back(&data->arg_lst, str);
-		}
-	}
-	trimmed = trimmed + i + 1;
-	return (trimmed);
-}
-
-/*
-** check the pipe TYPE and save it, if REDIR type, save target file and
-** move pointer forward ALSO MOVE INTO CMD->NEXT
-*/
-
-/*
-** check starting pos, see what the char is there Quote or Dquote
-** save from +1 of start until next occurence of Quote
-*/
 
 /*
 ** save until non alpha into args[i]
@@ -97,8 +34,6 @@ void	parse_args(t_data *data, char *trimmed)
 	ft_printf_fd(2, "remaining string_%s\n", trimmed);
 	if (*trimmed == D_QOTE || *trimmed == S_QOTE)
 		trimmed = ft_save_literal(data, (trimmed + 1), 0, *trimmed, trimmed);
-	else if (*trimmed == '$')
-		trimmed = ft_save_dolla(data, trimmed, 1);
 	else
 	{
 		str = ft_strldup(trimmed, ft_strchr_lib(trimmed, ' ') - trimmed);
--- src/parse_env.c
+++ src/parse_env.c
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/29 18:43:45 by averheij      #+#    #+#                 */
-/*   Updated: 2020/09/18 13:34:38 by averheij      ########   odam.nl         */
+/*   Updated: 2020/09/18 17:44:30 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,57 @@
 ** their value
 */
 
+//Search through string till $, this is pre-env-string
+//continue until end of arg or invalid char (check with ft_env_char), this is env var
+//anything afterward is post-env-string
+//	try to get env val based on var
+//	3strjoin pre, env-val, post
+
+int		ft_env_char(int c)
+{
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_');
+}
+
+char	*replace(t_data *data, char *str, int dollerdex)
+{
+	int		end;
+	char	*envvar;
+	char	*res;
+
+	str[dollerdex] = '\0';
+	envvar = str + dollerdex + 1;
+	end = 0;
+	while (envvar[end] && ft_env_char(envvar[end]))
+		end++;
+	if (end > 1)
+	{
+		envvar = get_env_val(envvar, data->env, end);
+		envvar = ft_strdup((envvar) ? envvar : "");
+		res = ft_3strjoin(str, envvar, str + dollerdex + 1 + end);
+		free(str);
+		free(envvar);
+		str = res;
+	}
+	return (str);
+}
+
 void	subtitute_enviroment_variables(t_data *data)
 {
 	int		i;
+	int		u;
 	char	*ptr;
 
 	i = 0;
 	while (i < data->argc)
 	{
-		if (data->argv[i][0] == '$' && ft_strlen(data->argv[i]) > 1)
+		u = 0;
+		while (data->argv[i] && data->argv[i][u])
 		{
-			ptr = &(data->argv[i][1]);
-			ptr = get_env_val(ptr, data->env, ft_strlen(ptr));
-			ptr = ft_strdup((ptr) ? ptr : "");
-			free(data->argv[i]);
-			data->argv[i] = ptr;
+			if (data->argv[i][u] == '$')
+				data->argv[i] = replace(data, data->argv[i], u);
+			u++;
 		}
+		ft_printf_fd(2, "arg%d:%s\n", i, data->argv[i]);
 		i++;
 	}
 }
