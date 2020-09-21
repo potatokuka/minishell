d91b79d89c6dd543361fd4aee4a43932797b67a0
MrOsteoporosis <sanderverheijen@outlook.com>
2020-09-21 13:26:54 +0200

    better env replace functions

--- src/parse_env.c
+++ src/parse_env.c
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/29 18:43:45 by averheij      #+#    #+#                 */
-/*   Updated: 2020/09/21 13:08:41 by averheij      ########   odam.nl         */
+/*   Updated: 2020/09/21 13:21:39 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,27 +28,44 @@ int		ft_env_char(int c)
 	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_');
 }
 
-char	*str_env_replace(t_data *data, char *str, int envstart)
+char	*str_env_replace_index(t_data *data, char *str, int envstart)
 {
-	int		end;
+	int		envend;
 	char	*envvar;
 	char	*res;
 
 	str[envstart] = '\0';
 	envvar = str + envstart + 1;
-	end = 0;
-	while (envvar[end] && ft_env_char(envvar[end]))
-		end++;
-	if (end > 1)
+	envend = 0;
+	while (envvar[envend] && ft_env_char(envvar[envend]))
+		envend++;
+	if (envend > 1)
 	{
-		envvar = get_env_val(envvar, data->env, end);
+		envvar = get_env_val(envvar, data->env, envend);
 		envvar = ft_strdup((envvar) ? envvar : "");
-		res = ft_3strjoin(str, envvar, str + envstart + 1 + end);
+		res = ft_3strjoin(str, envvar, str + envstart + 1 + envend);
 		free(str);
 		free(envvar);
 		str = res;
 	}
 	return (str);
+}
+
+char	*str_env_replace(t_data *data, char *str, int all)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			str = str_env_replace_index(data, str, i);
+			if (!all)
+				break;
+		}
+		i++;
+	}
 }
 
 void	argv_env_replace(t_data *data)
@@ -60,14 +77,7 @@ void	argv_env_replace(t_data *data)
 	i = 0;
 	while (i < data->argc)
 	{
-		u = 0;
-		while (data->argv[i] && data->argv[i][u])
-		{
-			if (data->argv[i][u] == '$')
-				data->argv[i] = str_env_replace(data, data->argv[i], u);
-			u++;
-		}
-		ft_printf_fd(2, "arg%d:%s\n", i, data->argv[i]);
+		str_env_replace_all(data, data->argv[i]);
 		i++;
 	}
 }
