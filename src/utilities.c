977485d6361b515f49f560073773718ba73e4360
griffin mbp <griffin.reed3@gmail.com>
2020-09-24 12:42:00 +0200

    Chore cun

--- src/utilities.c
+++ src/utilities.c
@@ -6,7 +6,7 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/19 18:05:49 by greed         #+#    #+#                 */
-/*   Updated: 2020/09/24 12:15:35 by greed         ########   odam.nl         */
+/*   Updated: 2020/09/24 12:41:47 by greed         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,21 @@
 void	ft_add_pid(t_pid *pid, int add_value, int add_status, t_data *data)
 {
 	int		i;
-	t_pid	*tmp;
+	int		*tmp;
+	int		*tmp_status;
 
-	tmp = ft_calloc(sizeof(pid), 1);
 	i = 0;
-	tmp->value = ft_calloc(sizeof(int*), pid->count + 2);
-	tmp->status = ft_calloc(sizeof(int*), pid->count + 2);
+	tmp = ft_calloc(sizeof(int), pid->count + 1);
+	tmp_status = ft_calloc(sizeof(int), pid->count + 1);
 	while (i < pid->count)
 	{
-		tmp->count += 1;
-		tmp->value[i] = ft_calloc(sizeof(int), 1);
 		tmp->value[i] = pid->value[i];
-		tmp->status[i] = ft_calloc(sizeof(int), 1);
 		tmp->status[i] = pid->status[i];
 		i++;
 	}
-	i += 1;
-	tmp->count = i;
-	tmp->value[i] = ft_calloc(sizeof(int), 1);
-	tmp->value[i] = &add_value;
-	tmp->status[i] = ft_calloc(sizeof(int), 1);
-	tmp->status[i] = &add_status;
-	free(pid);
+	pid->count = i + 1;
+	tmp = &add_value;
+	pid->status[i + 1] = &add_status;
 	data->pid = tmp;
 }
 
