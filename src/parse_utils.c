c1fb43f303526cf7e77a1158c558acab6b4f1988
MrOsteoporosis <sanderverheijen@outlook.com>
2020-09-25 14:46:08 +0200

    simplifying print_cmd_full

--- src/parse_utils.c
+++ src/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/03 20:45:35 by greed         #+#    #+#                 */
-/*   Updated: 2020/09/23 12:39:29 by greed         ########   odam.nl         */
+/*   Updated: 2020/09/25 13:53:37 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,30 +81,7 @@ void	print_cmd_full(t_data *data)
 	cmd = data->cmd;
 	while (cmd)
 	{
-		dprintf(2,"-- PRINT CUR CMD STRUCT --\n");
-		if (cmd->builtin)
-			dprintf(2,"Builtin_%s\n", cmd->builtin);
-		if (cmd->pipfd[IN] != -1)
-			dprintf(2,"Redir In_%d\n", cmd->pipfd[IN]);
-		if (cmd->pipfd[OUT] != -1)
-			dprintf(2,"Redir Out_%d\n", cmd->pipfd[OUT]);
-		if (cmd->pipfd2[WRITE_FD] != -1)
-			dprintf(2,"Pipe Write_%d\n", cmd->pipfd2[WRITE_FD]);
-		if (cmd->pipfd2[READ_FD] != -1)
-			dprintf(2,"Pipe Read_%d\n", cmd->pipfd2[READ_FD]);
-		if (cmd->tar_file)
-			dprintf(2,"Tar File_%s\n", cmd->tar_file);
-		if (cmd->argc)
-			dprintf(2,"Argc_%d\n", cmd->argc);
-		if (cmd->argv)
-		{
-			i = 0;
-			while (i < cmd->argc)
-			{
-				dprintf(2,"ARGV_%s\n",cmd->argv[i]);
-				i++;
-			}
-		}
+		print_cur_cmd(cmd);
 		cmd = cmd->next;
 	}
 }
