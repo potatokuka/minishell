f7bfeff60ba270a4685f694230839feae422e544
MrOsteoporosis <sanderverheijen@outlook.com>
2020-10-05 17:55:31 +0200

    more

--- src/ft_cd.c
+++ src/ft_cd.c
@@ -6,13 +6,13 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/20 12:42:16 by greed         #+#    #+#                 */
-/*   Updated: 2020/09/18 18:11:21 by greed         ########   odam.nl         */
+/*   Updated: 2020/10/05 17:48:35 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	cd_home(t_var *env)
+int		cd_home(t_var *env)
 {
 	char	*home;
 
@@ -21,19 +21,19 @@ void	cd_home(t_var *env)
 	if (!home)
 	{
 		ft_printf_fd(2, "cd: HOME not set\n");
-		return ;
+		return (1);
 	}
 	if (chdir(home) == -1)
 	{
 		ft_printf_fd(2, "cd: %s: %s\n", strerror(errno), home);
-		return ;
+		return (1);
 	}
 }
 
-void	ft_cd(t_cmd *cmd, t_var *env)
+int		ft_cd(t_cmd *cmd, t_var *env)
 {
 	if (cmd->argc == 0 || (ft_strcmp(cmd->argv[0], "~") == 0))
 		return (cd_home(env));
 	if (chdir(cmd->argv[0]) == -1)
-		return (error_builtin(cmd));
+		return (error_builtin(cmd, 1));
 }
