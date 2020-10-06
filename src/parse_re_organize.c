86705edf2b5eaabf507b9f09bc57b7cfd52e5a6b
griffin mbp <griffin.reed3@gmail.com>
2020-10-06 12:49:35 +0200

    3

--- src/parse_re_organize.c
+++ src/parse_re_organize.c
@@ -58,6 +58,8 @@ void			convert_esc(t_data *data, t_cmd *new, char *arg, int index)
 		}
 	}
 	ret = ft_strldup(tmp, x);
+	if (!ret)
+		put_error("Allocation Failed");
 	free(tmp);
 	dprintf(2, "Test RET_%s\n", ret);
 	lst_new_back(&new->arr_list, ft_strdup(ret));
