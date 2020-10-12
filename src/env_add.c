defe70ac688bfd0e5d7ceba46c7ca5067b0c9a25
MrOsteoporosis <sanderverheijen@outlook.com>
2020-10-12 12:44:01 +0200

    protection_env_add

--- src/env_add.c
+++ src/env_add.c
@@ -6,7 +6,7 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/22 23:10:44 by greed         #+#    #+#                 */
-/*   Updated: 2020/10/12 12:42:22 by averheij      ########   odam.nl         */
+/*   Updated: 2020/10/12 12:43:33 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,10 @@ t_var	*env_add(const char *name, const char *val)
 		return (NULL);
 	add->val = ft_strdup_lib(val);
 	if (!add->val)
+	{
+		free(add->name);
 		return (NULL);
+	}
 	add->next = NULL;
 	return (add);
 }
