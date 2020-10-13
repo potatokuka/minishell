a0d155d4e98402b53c3eb4b926f63d5fc3aa3d49
griffin mbp <griffin.reed3@gmail.com>
2020-10-13 17:48:12 +0200

    Chore: makes

--- include/minishell.h
+++ include/minishell.h
@@ -166,6 +166,7 @@ bool	ft_env_char(int c, int first);
 void	argv_env_replace(t_data *data);
 char	*str_env_replace(t_data *data, char *str, int all);
 char	*str_env_replace_index(t_data *data, char *str, int envstart);
+int		file_match(char file[], char *file2);
 
 int		parse_organize(t_data *data);
 int		clear_cmd(t_cmd *cmd, void(*del)(void *));
