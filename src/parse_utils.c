69caf6deeb15f829c9224d609eaa443baa7b0171
MrOsteoporosis <sanderverheijen@outlook.com>
2020-10-13 14:11:35 +0200

    moving print utils to print_Vars

--- src/parse_utils.c
+++ src/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/03 20:45:35 by greed         #+#    #+#                 */
-/*   Updated: 2020/10/05 14:05:48 by averheij      ########   odam.nl         */
+/*   Updated: 2020/10/13 14:04:03 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,46 +36,4 @@ int		drop_string(t_data *data, int i)
 		free(data->argv[i]);
 	data->argv[i] = NULL;
 	return (0);
-}
-
-void	print_cur_cmd(t_cmd *cmd)
-{
-	int		i;
-
-	i = 0;
-	dprintf(2,"-- PRINT CUR CMD STRUCT --\n");
-	if (cmd->builtin)
-		dprintf(2,"Builtin_%s\n", cmd->builtin);
-	if (cmd->io_fd[IN] != -1)
-		dprintf(2,"Redir In_%d\n", cmd->io_fd[IN]);
-	if (cmd->io_fd[OUT] != -1)
-		dprintf(2,"Redir Out_%d\n", cmd->io_fd[OUT]);
-	if (cmd->pipe_read_end != -1)
-		dprintf(2,"Pipe Read_%d\n", cmd->pipe_read_end);
-	if (cmd->tar_file)
-		dprintf(2,"Tar File_%s\n", cmd->tar_file);
-	if (cmd->argc)
-		dprintf(2,"Argc_%d\n", cmd->argc);
-	if (cmd->argv)
-	{
-		while (i < cmd->argc)
-		{
-			dprintf(2,"ARGV[%d]_%s\n", i, cmd->argv[i]);
-			i++;
-		}
-	}
-}
-
-void	print_cmd_full(t_data *data)
-{
-	t_cmd	*cmd;
-	int		i;
-
-	i = 0;
-	cmd = data->cmd;
-	while (cmd)
-	{
-		print_cur_cmd(cmd);
-		cmd = cmd->next;
-	}
 }
--- src/print_vars.c
+++ src/print_vars.c
@@ -6,7 +6,7 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/22 23:10:54 by greed         #+#    #+#                 */
-/*   Updated: 2020/10/12 14:42:49 by averheij      ########   odam.nl         */
+/*   Updated: 2020/10/13 14:04:04 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,4 +75,46 @@ void	test_args(char **argv, int argc)
 void	print_vars(t_data *data)
 {
 	print_args(data);
+}
+
+void	print_cur_cmd(t_cmd *cmd)
+{
+	int		i;
+
+	i = 0;
+	dprintf(2,"-- PRINT CUR CMD STRUCT --\n");
+	if (cmd->builtin)
+		dprintf(2,"Builtin_%s\n", cmd->builtin);
+	if (cmd->io_fd[IN] != -1)
+		dprintf(2,"Redir In_%d\n", cmd->io_fd[IN]);
+	if (cmd->io_fd[OUT] != -1)
+		dprintf(2,"Redir Out_%d\n", cmd->io_fd[OUT]);
+	if (cmd->pipe_read_end != -1)
+		dprintf(2,"Pipe Read_%d\n", cmd->pipe_read_end);
+	if (cmd->tar_file)
+		dprintf(2,"Tar File_%s\n", cmd->tar_file);
+	if (cmd->argc)
+		dprintf(2,"Argc_%d\n", cmd->argc);
+	if (cmd->argv)
+	{
+		while (i < cmd->argc)
+		{
+			dprintf(2,"ARGV[%d]_%s\n", i, cmd->argv[i]);
+			i++;
+		}
+	}
+}
+
+void	print_cmd_full(t_data *data)
+{
+	t_cmd	*cmd;
+	int		i;
+
+	i = 0;
+	cmd = data->cmd;
+	while (cmd)
+	{
+		print_cur_cmd(cmd);
+		cmd = cmd->next;
+	}
 }
