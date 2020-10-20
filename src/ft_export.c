5de0da547d6731ca53f106f0baa30b1e1a83bd5f
MrOsteoporosis <sanderverheijen@outlook.com>
2020-10-20 14:55:38 +0200

    export +=

--- src/ft_export.c
+++ src/ft_export.c
@@ -6,55 +6,77 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/22 23:09:44 by greed         #+#    #+#                 */
-/*   Updated: 2020/10/08 11:54:42 by averheij      ########   odam.nl         */
+/*   Updated: 2020/10/20 14:53:21 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		export2(t_cmd *cmd, t_var **env, size_t tmp, size_t a)
+int		validate_env_name(char *str)
 {
-	size_t i;
+	size_t		a;
 
-	i = 0;
-	while (cmd->argv[i])
+	a = 0;
+	while (str[a])
 	{
-		tmp = ft_strclen(cmd->argv[i], '=');
-		if (tmp < ft_strlen_lib(cmd->argv[i]))
+		if (!ft_env_char(str[a], !a))
 		{
-			a = 0;
-			while (a < tmp)
-			{
-				if (!ft_env_char(cmd->argv[i][a], !a))
-				{
-					ft_printf_fd(2, "%s: not a valid value\n", cmd->argv[i]);
-					return (1);
-				}
-				a++;
-			}
-			cmd->argv[i][tmp] = '\0';
-			if (env_set_val(cmd->argv[i], env, &cmd->argv[i][tmp + 1]))
-				return (2);
+			ft_printf_fd(2, "%s: not a valid value\n", str);
+			return (1);
 		}
-		i++;
+		a++;
 	}
-	return (0);
 }
 
 int		ft_export(t_cmd *cmd, t_var **env, char **envp)
 {
 	size_t	tmp;
-	int		a;
-	int		res;
+	size_t 	i;
 
 	if (cmd->argc == 0)
 	{
 		ft_env(envp);
 		return (0);
 	}
-	res = export2(cmd, env, tmp, a);
-	if (res)
-		return (res);
+	i = 0;
+	while (cmd->argv[i])
+	{
+		tmp = ft_strclen(cmd->argv[i], '=');
+		if (tmp < ft_strlen_lib(cmd->argv[i]))
+		{
+			char	*val;
+
+			if (tmp - 1 >= 0 && cmd->argv[i][tmp - 1] == '+')
+			{
+				cmd->argv[i][tmp - 1] = '\0';
+				if (validate_env_name(cmd->argv[i]))
+					return (1);
+				val = get_env_val(cmd->argv[i], *env, ft_strlen(cmd->argv[i]));
+				if (!val)
+					val = "";
+				dprintf(2, "%s\t%s\n", val, &cmd->argv[i][tmp + 1]);
+				val = ft_strjoin(val, &cmd->argv[i][tmp + 1]);
+				dprintf(2, "%s\n", val);
+				if (!val)
+					return (2);
+				if (env_set_val(cmd->argv[i], env, val))
+				{
+					free(val);
+					return (2);
+				}
+				free(val);
+			}
+			else
+			{
+				cmd->argv[i][tmp] = '\0';
+				if (validate_env_name(cmd->argv[i]))
+					return (1);
+				if (env_set_val(cmd->argv[i], env, &cmd->argv[i][tmp + 1]))
+					return (2);
+			}
+		}
+		i++;
+	}
 	cmd->update_env = 1;
 	return (0);
 }
