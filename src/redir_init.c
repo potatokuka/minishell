80054c2b4004482b02536a2e9d5209dcdc3e3f88
griffin mbp <griffin.reed3@gmail.com>
2020-10-23 12:33:34 +0200

    Chore: yup yes today

--- src/redir_init.c
+++ src/redir_init.c
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/30 10:57:24 by averheij      #+#    #+#                 */
-/*   Updated: 2020/10/21 17:30:53 by averheij      ########   odam.nl         */
+/*   Updated: 2020/10/23 12:26:29 by greed         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int		redir_dispatch(t_data *data, t_fd_sto *fd, t_cmd *cmd, char *pipe)
 {
 	if (cmd && pipe)
 	{
-		/*dprintf(2, "pipe =%s\n", pipe);*/
 		if (ft_strcmp(pipe, ">>") == 0)
 		{
 			if (redir_append(data, fd, cmd))
