568d6822d6e650ec712f93b253413727217cf7ba
MrOsteoporosis <sanderverheijen@outlook.com>
2020-10-26 17:54:28 +0100

    improved echo

--- src/ft_echo.c
+++ src/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/20 10:17:20 by greed         #+#    #+#                 */
-/*   Updated: 2020/10/01 13:40:39 by averheij      ########   odam.nl         */
+/*   Updated: 2020/10/26 17:41:36 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,22 @@ void	ft_echo(t_cmd *cmd)
 
 	flag = 0;
 	i = 0;
-	if (cmd->argc != 0 && ft_strcmp(cmd->argv[0], "-n") == 0)
-	{
-		flag += 1;
-		i++;
-	}
 	while (i < cmd->argc)
 	{
-		ft_printf_fd(1, "%s", cmd->argv[i]);
-		if (cmd->argc - i == 1)
-		{
-			if (flag == 1)
-				break ;
-			ft_printf_fd(1, "\n");
-		}
+		if (ft_strcmp(cmd->argv[i], "-n") == 0 && flag == i)
+			flag += 1;
 		else
-			ft_printf_fd(1, " ");
+		{
+			ft_printf_fd(1, "%s", cmd->argv[i]);
+			if (cmd->argc - i == 1)
+			{
+				if (flag >= 1)
+					break ;
+				ft_printf_fd(1, "\n");
+			}
+			else
+				ft_printf_fd(1, " ");
+		}
 		i++;
 	}
-	return ;
 }
