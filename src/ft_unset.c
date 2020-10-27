49d6f1eb378a6b6c10b3fc02c21247a578e84f7e
MrOsteoporosis <sanderverheijen@outlook.com>
2020-10-27 11:40:07 +0100

    using return values in usnet

--- src/ft_unset.c
+++ src/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/22 13:43:46 by greed         #+#    #+#                 */
-/*   Updated: 2020/10/13 17:11:27 by averheij      ########   odam.nl         */
+/*   Updated: 2020/10/27 11:38:44 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,19 @@
 int		ft_unset(t_cmd *cmd, t_var **env)
 {
 	int		i;
+	int		ret;
 
+	ret = 0;
 	if (cmd->argc < 1)
 		return (0);
 	i = 0;
 	while (cmd->argv[i])
 	{
-		unset_env(env, cmd->argv[i]);
+		ret = unset_env(env, cmd->argv[i]);
+		if (ret)
+			ft_printf_fd(2, "Error: unset: '%s' is invalid\n", cmd->argv[i]);
 		i++;
 	}
 	cmd->update_env = 1;
-	return (0);
+	return (ret);
 }
