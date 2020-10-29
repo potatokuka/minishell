7cf3828982723a55a1cf76a4a25744e3140f3f17
MrOsteoporosis <sanderverheijen@outlook.com>
2020-10-29 15:10:19 +0100

    unset edge cases :)

--- src/ft_unset.c
+++ src/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/22 13:43:46 by greed         #+#    #+#                 */
-/*   Updated: 2020/10/27 11:38:44 by averheij      ########   odam.nl         */
+/*   Updated: 2020/10/29 15:10:08 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,16 @@
 /*
 ** send each argv to get removed from ENV if they exist
 */
+
+int		is_valid_env_name(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] && ft_env_char(str[i], (i == 0) ? 1 : 0))
+		i++;
+	return (!str[i]);
+}
 
 int		ft_unset(t_cmd *cmd, t_var **env)
 {
@@ -27,9 +37,13 @@ int		ft_unset(t_cmd *cmd, t_var **env)
 	i = 0;
 	while (cmd->argv[i])
 	{
-		ret = unset_env(env, cmd->argv[i]);
-		if (ret)
+		if (!is_valid_env_name(cmd->argv[i]))
+		{
+			ret = 1;
 			ft_printf_fd(2, "Error: unset: '%s' is invalid\n", cmd->argv[i]);
+		}
+		else
+			unset_env(env, cmd->argv[i]);
 		i++;
 	}
 	cmd->update_env = 1;
