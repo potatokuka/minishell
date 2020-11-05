6991c6927d66bb401cd97beffdef7d9944c4b486
MrOsteoporosis <sanderverheijen@outlook.com>
2020-11-05 14:45:23 +0100

    unused prototype

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: greed <greed@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/14 15:36:44 by greed         #+#    #+#                 */
-/*   Updated: 2020/11/05 13:19:11 by averheij      ########   odam.nl         */
+/*   Updated: 2020/11/05 14:45:06 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,7 +149,6 @@ char				*ft_3strjoin(const char *str1, const char *str2,
 char				**free_array_null(char **str);
 size_t				ft_strclen(const char *str, int c);
 int					get_path(char **res);
-void				ft_reset_pid(t_pid *pid);
 int					ft_add_pid(t_pid *pid, int add_value);
 int					sto_fd(t_fd_sto *fd, int add_fd);
 void				close_fd(t_fd_sto *fd, int io_fd[2]);
